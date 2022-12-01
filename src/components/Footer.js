985a2ca0045ace8bb50375eec0f6f3ea79c3d8a9
312 <a312@312s-MBP.broadband>
2022-12-01 14:49:40 +0000

    footer v1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,8 @@
 const Footer = () => (
   <footer className="footer">
-    <div className="content has-text-centered">
-      <p>
-        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>
-        . The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-        website content is licensed{' '}
-        <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-          CC BY NC SA 4.0
-        </a>
-        .
+    <div className="content size-3 has-text-centered">
+      <p className="title is-4 has-text-centered has-text-white">
+        <strong>©</strong> Nathan Harris & Ulas Temel 2022
       </p>
     </div>
   </footer>
