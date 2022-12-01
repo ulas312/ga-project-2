2bb8337282bee37a78a7d10d52743d0802c59288
312 <a312@312s-MBP.broadband>
2022-12-01 12:46:05 +0000

    simple homepage v1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 const Home = () => (
-  <section className="hero is-fullheight-with-navbar is-warning">
+  <section className="hero is-fullheight-with-navbar ">
     <div className="hero-body">
       <div className="container">
-        <p className="title is-1 has-text-centered has-text-black">
-          <span>FBI MOST WANTED</span>
-        </p>
+        {/* <p className="title is-1 has-text-centered has-text-black">
+          <h1 className="hero-text">FBI MOST WANTED</h1>
+        </p> */}
       </div>
     </div>
   </section>
