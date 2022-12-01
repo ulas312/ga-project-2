cabad82266773e1d8a4b2927b3d973fd1e1c0589
312 <a312@312s-MBP.broadband>
2022-12-01 23:45:34 +0000

    pulled wantedcard

--- src/components/WantedIndex.js
+++ src/components/WantedIndex.js
@@ -1,10 +1,13 @@
 // import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { getAllSuspects } from '../lib/api';
+
+// import WantedCard from '../components/WantedCard';
+
 // const BASE_URL = 'https://api.fbi.gov/wanted/v1/list';
 
 function WantedIndex() {
-  console.log('gfsdfg');
+  console.log('test');
   const [suspects, setSuspects] = useState(null);
 
   useEffect(() => {
@@ -12,12 +15,24 @@ function WantedIndex() {
       .then((res) => setSuspects(res.data))
       .catch((err) => console.log.error(err));
   }, []);
-  console.log(suspects.items[0]);
+  // console.log(suspects.items[0]);
   if (suspects === null) {
     return <p>Loading Most Wanted...</p>;
   }
 
-  return <p>Suspects</p>;
+  // return <p>Suspects</p>;
+
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns is-multiline">
+          {/* {suspects.map((suspect) => (
+            <WantedCard key={suspect._id} {...suspect} />
+          ))} */}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default WantedIndex;
