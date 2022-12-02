55f886602e8bc0d93c78ded288b14c12f3b4c088
312 <a312@312s-MBP.broadband>
2022-12-02 01:26:50 +0000

    v2 footer with styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/FBI-logo.png';
 
 const Navbar = () => (
-  <nav className="navbar is-dark">
+  <nav className="navbar ">
     <div className="container">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          Home
-        </Link>
-        <Link to="/wantedList" className="navbar-item">
-          Wanted List
-        </Link>
+        <a className="navbar-item" href="/">
+          <img src={logo} width="200" />
+        </a>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-end">
+          <Link to="/" className="navbar-item has-text-white">
+            Home
+          </Link>
+          <Link to="/wantedList" className="navbar-item has-text-white">
+            Wanted List
+          </Link>
+        </div>
       </div>
     </div>
   </nav>
