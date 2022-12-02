b234da4eaf531f1b2c03be7a21219c1ea4ddeb0c
Nathan Harris <nathan.harris.1@outlook.com>
2022-12-02 10:50:10 +0000

    connects wanted card to api call in wanted index

--- src/components/WantedCard.js
+++ src/components/WantedCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 
 // const WantedCard = ({ name, image, category, _id }) => (
-const WantedCard = ({ name, image, category }) => (
+const WantedCard = ({ name, image }) => (
   <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
     {/* <Link to={`/wantedList/${_id}`}> */}
     <div className="card">
@@ -14,11 +14,12 @@ const WantedCard = ({ name, image, category }) => (
           <img src={image} alt={name} loading="lazy" width="255" height="255" />
         </figure>
       </div>
-      <div className="card-content">
-        <h5 className="">{category}</h5>
-      </div>
     </div>
     {/* </Link> */}
   </div>
 );
 export default WantedCard;
+
+// <div className="card-content">
+//   <h5 className="">{category}</h5>
+// </div>;
--- src/components/WantedIndex.js
+++ src/components/WantedIndex.js
@@ -2,33 +2,35 @@
 import { useState, useEffect } from 'react';
 import { getAllSuspects } from '../lib/api';
 
-// import WantedCard from '../components/WantedCard';
+import WantedCard from '../components/WantedCard';
 
 // const BASE_URL = 'https://api.fbi.gov/wanted/v1/list';
 
 function WantedIndex() {
-  console.log('test');
   const [suspects, setSuspects] = useState(null);
 
   useEffect(() => {
     getAllSuspects()
       .then((res) => setSuspects(res.data))
-      .catch((err) => console.log.error(err));
+      .catch((err) => console.error(err));
   }, []);
-  // console.log(suspects.items[0]);
+
   if (suspects === null) {
     return <p>Loading Most Wanted...</p>;
   }
-
-  // return <p>Suspects</p>;
 
   return (
     <section className="section">
       <div className="container">
         <div className="columns is-multiline">
-          {/* {suspects.map((suspect) => (
-            <WantedCard key={suspect._id} {...suspect} />
-          ))} */}
+          {suspects.items.map((suspect) => (
+            <WantedCard
+              key={suspect.title}
+              // key={suspect._id}
+              image={suspect.images[0].thumb}
+              name={suspect.title}
+            />
+          ))}
         </div>
       </div>
     </section>
