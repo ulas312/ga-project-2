464c01cf37a2dd414846ab3db0737252e37d2bd4
Nathan Harris <nathan.harris.1@outlook.com>
2022-12-02 13:33:19 +0000

    returns api data from single wanted request

--- src/components/WantedShow.js
+++ src/components/WantedShow.js
@@ -1,7 +1,23 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getSingleSuspect } from '../lib/api';
 
 const WantedShow = () => {
   const { id } = useParams();
+  const [wanted, setWanted] = useState(null);
+
+  useEffect(() => {
+    getSingleSuspect(id)
+      .then((res) => setWanted(res.data))
+      .catch((err) => console.error(err));
+  }, [id]);
+
+  if (wanted === null) {
+    return <p>Loading....</p>;
+  }
+
+  console.log(wanted);
+
   return (
     <>
       <p>wanted show is working</p>
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.fbi.gov/wanted/v1/list';
+// const BASE_URL = 'https://api.fbi.gov/wanted/v1/list';
 
-export const getAllSuspects = () => axios.get(BASE_URL);
+// export const getAllSuspects = () => axios.get(BASE_URL);
+const BASE_URL = 'https://api.fbi.gov';
 
-// export const getSingleSuspect = (suspectUId) =>
-//   axios.get(`${BASE_URL}/wantedList/${suspectUId}`);
+export const getAllSuspects = () => axios.get(`${BASE_URL}/wanted/v1/list`);
+
+export const getSingleSuspect = (UId) =>
+  axios.get(`${BASE_URL}/@wanted-person/${UId}`);
