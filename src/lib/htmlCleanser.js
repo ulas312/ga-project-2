1c30e6e73a20d2c1356e6da98e9d5ff06fabd33a
Nathan Harris <nathan.harris.1@outlook.com>
2022-12-02 18:29:12 +0000

    fixes small error with export for cleanse file

--- src/lib/htmlCleanser.js
+++ src/lib/htmlCleanser.js
@@ -1,6 +1,6 @@
 // const testString = `<p>On August 1, 2019, at approximately 10:00 a.m., a GardaWorld armored car was attacked at gunpoint at 3535 Market Street in Philadelphia, Pennsylvania's University City section.</p>\r\n<p><br /> <br />Two armed robbers and a getaway driver were observed.  Armed Robber 1 was in possession of a semi-automatic rifle loaded with a drum-style magazine and Armed Robber 2 was in possession of a loaded semi-automatic handgun.</p>\r\n<p><br /> <br />Video from surveillance cameras in the area of the attempted robbery shows the subjects arriving on the scene in their vehicle, two armed robbers advancing towards the armored car, and then fleeing upon encountering gunfire from the armored car personnel.  Armed Robber 1 is seen diving into the vehicle, with the driver taking off, and Armed Robber 2 then fleeing on foot.</p>\r\n<p><br /> <br />Armed Robber 2 has been arrested and charged with attempted robbery which interferes with interstate commerce in connection with this incident.  He is in federal custody.  The FBI seeks information about the remaining individuals.</p>\r\n<p> </p>`;
 
-function cleanseData(string, toFilter = ["<p>", "</p>", "<br />"]) {
+function cleanseData(string, toFilter = ['<p>', '</p>', '<br />']) {
   let cleansedString = string;
   for (tagElement of toFilter) {
     let index = cleansedString.indexOf(tagElement);
@@ -8,17 +8,17 @@ function cleanseData(string, toFilter = ["<p>", "</p>", "<br />"]) {
       if (index == 0) {
         cleansedString = [...cleansedString]
           .splice(index + tagElement.length, cleansedString.length)
-          .join("");
+          .join('');
       } else {
         cleansedString =
-          [...cleansedString].splice(0, index - 1).join("") +
+          [...cleansedString].splice(0, index - 1).join('') +
           [...cleansedString]
             .splice(index + tagElement.length, cleansedString.length)
-            .join("");
+            .join('');
       }
       index = cleansedString.indexOf(tagElement);
     }
   }
   return cleansedString;
 }
-return default cleanseData
+export default cleanseData;
