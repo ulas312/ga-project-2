449bd0cf868d4f4cae0a5d336c975f9e040c4df3
312 <a312@312s-MBP.broadband>
2022-12-03 16:24:48 +0000

    fixed styling for logo and made footer slightly smaller

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,20 @@ const Navbar = () => (
   <nav className="navbar ">
     <div className="container">
       <div className="navbar-brand">
-        <a className="navbar-item" href="/">
-          <img src={logo} width="200" />
+        <a className="navbar-item logo" href="/">
+          <img src={logo} style={{ minWidth: 80, minHeight: 80 }} />
+        </a>
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
       </div>
       <div id="navbarBasicExample" className="navbar-menu">
